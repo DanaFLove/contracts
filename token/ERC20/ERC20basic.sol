0b37968fc8dbcd8466de3b38481d64368c6579a8
Dana Love <danaflove@me.com>
2022-08-05 10:18:56 -0700

    Update ERC20basic.sol

--- token/ERC20/ERC20basic.sol
+++ token/ERC20/ERC20basic.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.4;
 /**
  * @title ERC20Basic
  * @dev Fundamental function set for ERC20 interface. See also https://github.com/ethereum/EIPs/issues/179
+ * @dev To be called with most every SC
  */
 
 contract ERC20Basic {
