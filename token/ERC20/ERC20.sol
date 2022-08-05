d03f1feb54c4aff462310b87d264802ce2db7360
Dana Love <danaflove@me.com>
2022-08-05 10:22:27 -0700

    Create ERC20.sol

---
+++ token/ERC20/ERC20.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.23;
+
+import "./ERC20Basic.sol";
+
+
+/**
+ * @title ERC20 interface
+ * @dev Note we import the basictoken here
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender)
+    public view returns (uint256);
+
+  function transferFrom(address from, address to, uint256 value)
+    public returns (bool);
+
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(
+    address indexed owner,
+    address indexed spender,
+    uint256 value
+  );
+}
