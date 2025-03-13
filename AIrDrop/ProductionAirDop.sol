1c36bfb68726213728ca9890f83c0df14550e501
Dana Love <danaflove@me.com>
2025-03-13 10:52:30 -0700

    Update ProductionAirDop.sol

--- AIrDrop/ProductionAirDop.sol
+++ AIrDrop/ProductionAirDop.sol
@@ -13,3 +13,20 @@ contract Token is ERC20 {
     // Use ERC20 from OpenZeppelin for safe transfers
 }
 
+/**
+ * @dev The transferOwnership function is inherited from OpenZepplin’s Ownable contract, allowing the owner to transfer ownership to a new address.
+ */
+
+contract AirDrop is Ownable, ReentrancyGuard {
+    Token public tokenInstance;
+}
+
+    /**
+     * @dev Performs an airdrop of tokens to specified addresses.
+     * @param _addresses Array of recipient addresses.
+     * @param _tokenAmount Amount of tokens to transfer to each address.
+     * @param _ethAmount Amount of Ether to transfer if the recipient's balance is zero.
+     * @return bool Whether the airdrop was successful.
+     */
+    function doAirDrop(address[] memory _addresses, uint256 _tokenAmount, uint256 _ethAmount) onlyOwner public nonReentrant returns (bool) {
+        require(_addresses.length > 0, "Addresses array cannot be empty");
