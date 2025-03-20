4df31cb0e44fa38bdff34d1f59683ffb377d54bf
Dana Love <danaflove@me.com>
2025-03-20 13:29:34 -0700

    Update stake_contract.sol

--- stake_engine/stake_contract.sol
+++ stake_engine/stake_contract.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 interface IERC20 {
     function transfer(address to, uint256 amount) external returns (bool);
     function approve(address spender, uint256 amount) external returns (bool);
